898501d416be224d1988d8f5c2416e2064b62da7
Lucas <lucas.corbalan2002@gmail.com>
2025-06-22 22:30:07 -0300

    Login

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -1,13 +1,14 @@
 
 import Login from '../Components/Login'
-
+import Header from '../Components/Header'
+import Footer from '../Components/Footer'
 
 const LoginPage = () => {
   return (
     <div>
-      
+        <Header />
         <Login />
-     
+        <Footer />
     </div>
   )
 }
